37f8eb4cca94612902a61032ca386a685ac62714
NikitaZuev <bridgez712@gmail.com>
2021-09-23 00:37:56 +0700

    master

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
+  var wrapperMenu = document.querySelector('.menu');
+  var wrapperMenuOpen = document.querySelector('.menu__box');
 
+  wrapperMenu.addEventListener('click', function(){
+    wrapperMenu.classList.toggle('open');  
+    wrapperMenuOpen.classList.toggle('open');  
+  });
+  
 })
