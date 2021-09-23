1280ebc491ed785ff56907274beefca432a006a2
NikitaZuev <bridgez712@gmail.com>
2021-09-23 22:16:19 +0700

    master

--- js/main.js
+++ js/main.js
@@ -6,5 +6,11 @@ $(document).ready(function(){
     wrapperMenu.classList.toggle('open');  
     wrapperMenuOpen.classList.toggle('open');  
   });
-  
 })
+$(document).ready(function () {
+  $('.service__info').mouseover(           
+    function(){ //при наведении на любую ссылку указанного class'a  
+         $('.service__info').removeClass('service__info_link--active'); // у всех удаляем class
+         $(this).addClass('service__info_link--active') }  // а этой ссылке добавляем
+  );     
+});
