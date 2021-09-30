656bfd6a9b08756c5821b5d57f0e5e2c1b579dac
NikitaZuev <bridgez712@gmail.com>
2021-09-30 17:10:42 +0700

    Вращение при наведении

--- js/main.js
+++ js/main.js
@@ -14,3 +14,34 @@ $(document).ready(function () {
          $(this).addClass('service__info_link--active') } // а этой ссылке добавляем
   );     
 });
+/*document.addEventListener('mousemove', function (event) {
+  var div = document.querySelector(".circle");
+  var bb = div.getBoundingClientRect();
+  var cx = bb.left + bb.width / 2, cy = bb.top + bb.height / 2;
+  var angle = Math.atan2(event.y - cy, event.x - cx);
+  
+  div.style.transform = "rotate(" + angle + "rad)"
+})*/
+$(document).ready(function(){
+  var div = document.querySelector(".circle");
+  $('.one').mouseenter(
+    function(){
+        div.style.transform = "rotate(" + -0.1 + "rad)"}
+  )
+  $('.two').mouseenter(
+    function(){
+        div.style.transform = "rotate(" + -0.6 + "rad)"}
+  )
+  $('.three').mouseenter(
+    function(){
+        div.style.transform = "rotate(" + -1.1 + "rad)"}
+  )
+  $('.four').mouseenter(
+    function(){
+        div.style.transform = "rotate(" + -1.6 + "rad)"}
+  )
+  $('.five').mouseenter(
+    function(){
+        div.style.transform = "rotate(" + -2.1 + "rad)"}
+  )
+})
